6f32f69635c2d4ad8246894deb91c7821f46a564
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-04-22 08:21:09 +0200

    Adding pricing component

--- src/components/pricing/index.js
+++ src/components/pricing/index.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
+import MyButton from '../utills/MyButton';
+import Zoom from 'react-reveal/Zoom';
 
 class Pricing extends Component {
 
     state = {
-        prices:[100, 150, 250],
-        positions:['Balcony','Medium', 'Star'],
-        desc:[
+        prices: [100, 150, 250],
+        positions: ['Balcony', 'Medium', 'Star'],
+        desc: [
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id elit eget erat pretium malesuada.',
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id elit eget erat pretium malesuada.',
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id elit eget erat pretium malesuada.'
         ],
-        linkto:['http://sales/b','http://sales/m','http://sales/s']
+        linkto: ['http://sales/b', 'http://sales/m', 'http://sales/s'],
+        delay: [500, 0, 500]
     }
 
     showBoxes = () => (
-        <div>
-            hello
-        </div>
+        this.state.prices.map((box, i) => (
+            <Zoom delay={this.state.delay[i]} key={i}>
+                <div className="pricing_item">
+                    <div className="pricing_inner_wrapper">
+                        <div className="pricing_title">
+                            <span>${this.state.prices[i]}</span>
+                            <span>{this.state.positions[i]}</span>
+                        </div>
+                        <div className="pricing_description">
+                            {this.state.desc[i]}
+                        </div>
+                        <div className="pricing_buttons">
+                            <MyButton
+                                text="Purchase"
+                                bck="#ffa800"
+                                color="#ffffff"
+                                link={this.state.linkto[i]}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </Zoom>
+
+        ))
     )
 
     render() {
